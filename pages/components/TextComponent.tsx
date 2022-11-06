7fdac54f9d6c8867376139be050a2495e22d896f
김명원 <auddnjs2008@naver.com>
2022-11-06 14:16:13 +0900

    husky test

--- pages/components/TextComponent.tsx
+++ pages/components/TextComponent.tsx
@@ -3,6 +3,7 @@ import testRecoil from '../recoil/testRecoil';
 import { useSetRecoilState } from "recoil";
 
 const TextComponent = () => {
+    console.log("husky testest");
     const setError = useSetRecoilState(testRecoil);
     const { data, error } = useQuery("testApi", () => Promise.reject({ message: "OMG" }), {
         retry: 1,
